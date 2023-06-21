70ce695764115942f2d68b9c17fd4f5e1a6420e5
adyze <115144204+adamurbanek@users.noreply.github.com>
2023-06-21 12:48:56 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -4,8 +4,8 @@ radio.setFrequencyBand(7);
 let levaStrana;
 let pravaStrana;
 
-let speed = 140;
-let speedCorrect = -10;
+let speed = 180;
+let speedCorrect = -120;
 let speedTurn = 140;
 let krizovatkaTurn = 140;
 
@@ -23,74 +23,71 @@ radio.onReceivedNumber(function (num: number) {
 
 function setSpeed(speedLeft: number, speedRight: number) {
     PCAmotor.MotorRun(PCAmotor.Motors.M1, speedLeft);
-    PCAmotor.MotorRun(PCAmotor.Motors.M4, speedRight * 0.75);
+    PCAmotor.MotorRun(PCAmotor.Motors.M4, speedRight);
 }
 
 let tick = 0;
-let lastData: number[][];
-const dir = {LEFT: 0, CENTER: 1, RIGHT: 2};
+let lastData: number[][] = [[], [], []];
+const dir = { LEFT: 0, CENTER: 1, RIGHT: 2 };
 
 function dataHistory(direction: number, state: boolean) {
     if (tick % 2 == 0) {
         lastData[direction].push(state ? 1 : 0);
-        if (lastData[direction].length > 10) {
+        if (lastData[direction].length > 15) {
             lastData[direction].shift();
         }
     }
 }
+
+function readHistory(direction: number) {
+    let modus = 0
+    for (const x of lastData[direction]) {
+        modus += x
+    }
+
+    if (Math.round(modus / 15) === 1) {
+        return true
+    } else {
+        return false
+    }
+
+}
+
+let count = 0
 
 basic.forever(() => {
     let center = (whiteline ^ pins.digitalReadPin(pinC)) == 0 ? false : true;
     let left = (whiteline ^ pins.digitalReadPin(pinL)) == 0 ? false : true;
     let right = (whiteline ^ pins.digitalReadPin(pinR)) == 0 ? false : true;
 
-    if (dataHistory(dir.CENTER, center)) {
-        if (dataHistory(dir.LEFT, left) && dataHistory(dir.RIGHT, right)) {
-            setSpeed(speed, speed);
-            // if (direction == -1) {
-            //     setSpeed(-krizovatkaTurn, krizovatkaTurn);
-            //     basic.pause(850);
-            //     setSpeed(speed, speed);
-            //     basic.pause(850);
-            // } else if (direction == 1) {
-            //     setSpeed(krizovatkaTurn, -krizovatkaTurn);
-            //     basic.pause(850);
-            //     setSpeed(speed, speed);
-            //     basic.pause(850);
-            // } else {
-            //     setSpeed(speed, speed);
-        }
-        if (dataHistory(dir.LEFT, left) && dataHistory(dir.RIGHT, right)) {
-            if (direction == -1) {
-                setSpeed(-krizovatkaTurn, krizovatkaTurn);
-                basic.pause(850);
-                setSpeed(speed, speed);
-                basic.pause(850);
-            } else if (direction == 1) {
-                setSpeed(krizovatkaTurn, -krizovatkaTurn);
-                basic.pause(850);
-                setSpeed(speed, speed);
-                basic.pause(850);
-            } else {
-                setSpeed(speed, speed);
-            }
-        } else {
-            if (dataHistory(dir.RIGHT, right)) {
-                pravaStrana = speed;
-            } else {
-                pravaStrana = speedCorrect;
-            }
-
-            if (dataHistory(dir.LEFT, left)) {
-                levaStrana = speed;
-            } else {
-                levaStrana = speedCorrect;
-            }
-            setSpeed(levaStrana, pravaStrana);
-        }
-        setSpeed(-speed, -speed);
-        basic.pause(300);
+    while (count < 15) {
+        dataHistory(dir.LEFT, left);
+        dataHistory(dir.CENTER, center);
+        dataHistory(dir.RIGHT, right);
+        count++;
     }
-    basic.pause(50);
+        setSpeed(speed, speed);  
+
+        if (right) {
+            pravaStrana = speed;
+        } else {
+            pravaStrana = speedCorrect;
+        }
+
+        if (left) {
+            levaStrana = speed;
+        } else {
+            levaStrana = speedCorrect;
+        }
+        setSpeed(levaStrana, pravaStrana);
+            
+        if(left && right){
+            setSpeed(speed, speed);
+        }
+            
+    center = readHistory(dir.CENTER);
+    left = readHistory(dir.LEFT);
+    right = readHistory(dir.RIGHT);
+
     tick += 1;
 })
